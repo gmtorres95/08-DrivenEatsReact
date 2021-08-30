16f108d2295e233d886767d2ef67c3976a000861
Gabriel Torres <al2012.163.torres@gmail.com>
2021-08-30 02:12:40 -0300

    Refactored arrays

--- src/components/App.js
+++ src/components/App.js
@@ -1,150 +1,150 @@
 import { Category } from "./Category";
-import { Footer } from "./Footer";
+import { FinishOrder } from "./FinishOrder";
 import { Header } from "./Header";
 
-export default function App () {
-    const dishes = [
+export default function App() {
+    const menu = [
         {
-            img: "/media/pratos/strogonoff.jpeg",
-            name: "Strogonoff de Frango",
-            description: "Arroz, strogonoff de frango e batata palha.",
-            price: 13.9,
-            amount: 0
+            title: "Primeiro, seu prato",
+            options: [
+                {
+                    img: "/media/pratos/strogonoff.jpeg",
+                    name: "Strogonoff de Frango",
+                    description: "Arroz, strogonoff de frango e batata palha.",
+                    price: 13.9,
+                    amount: 0
+                },
+                {
+                    img: "/media/pratos/parmegiana.jpeg",
+                    name: "Frango à Parmegiana",
+                    description: "Arroz, fritas, frango à parmegiana e salada.",
+                    price: 15.4,
+                    amount: 0
+                },
+                {
+                    img: "/media/pratos/milanesa.jpeg",
+                    name: "Frango à Milanesa",
+                    description: "Arroz, farofa, fritas e frango à milanesa.",
+                    price: 15.9,
+                    amount: 0
+                },
+                {
+                    img: "/media/pratos/grelhado.jpeg",
+                    name: "Frango Grelhado",
+                    description: "Arroz, feijão, farofa e frango grelhado.",
+                    price: 14.9,
+                    amount: 0
+                },
+                {
+                    img: "/media/pratos/liguiça.jpeg",
+                    name: "Linguiça de Lombo",
+                    description: "Arroz, feijão, farofa, linguiça de lombo acebolada e salada.",
+                    price: 14.4,
+                    amount: 0
+                },
+                {
+                    img: "/media/pratos/peixe.jpeg",
+                    name: "Peixe à Milanesa",
+                    description: "Arroz, fritas, filé de peixe à milanesa e salada.",
+                    price: 19.9,
+                    amount: 0
+                }
+            ]
         },
         {
-            img: "/media/pratos/parmegiana.jpeg",
-            name: "Frango à Parmegiana",
-            description: "Arroz, fritas, frango à parmegiana e salada.",
-            price: 15.4,
-            amount: 0
+            title: "Agora, sua bebida",
+            options: [
+                {
+                    img: "/media/bebidas/coca.jpeg",
+                    name: "Coca-Cola",
+                    description: "Lata de 350ml.",
+                    price: 5,
+                    amount: 0
+                },
+                {
+                    img: "/media/bebidas/kuat.jpeg",
+                    name: "Guaraná Kuat",
+                    description: "Lata de 350ml.",
+                    price: 4,
+                    amount: 0
+                },
+                {
+                    img: "/media/bebidas/sprite.jpeg",
+                    name: "Sprite",
+                    description: "Lata de 350ml.",
+                    price: 4.5,
+                    amount: 0
+                },
+                {
+                    img: "/media/bebidas/mate.jpeg",
+                    name: "Chá Gelado Mate",
+                    description: "Garaffa de 450ml sabor limão.",
+                    price: 6,
+                    amount: 0
+                },
+                {
+                    img: "/media/bebidas/agua.jpeg",
+                    name: "Água Mineral",
+                    description: "Garaffa de 500ml sem gás.",
+                    price: 1,
+                    amount: 0
+                }
+            ]
         },
         {
-            img: "/media/pratos/milanesa.jpeg",
-            name: "Frango à Milanesa",
-            description: "Arroz, farofa, fritas e frango à milanesa.",
-            price: 15.9,
-            amount: 0
-        },
-        {
-            img: "/media/pratos/grelhado.jpeg",
-            name: "Frango Grelhado",
-            description: "Arroz, feijão, farofa e frango grelhado.",
-            price: 14.9,
-            amount: 0
-        },
-        {
-            img: "/media/pratos/liguiça.jpeg",
-            name: "Linguiça de Lombo",
-            description: "Arroz, feijão, farofa, linguiça de lombo acebolada e salada.",
-            price: 14.4,
-            amount: 0
-        },
-        {
-            img: "/media/pratos/peixe.jpeg",
-            name: "Peixe à Milanesa",
-            description: "Arroz, fritas, filé de peixe à milanesa e salada.",
-            price: 19.9,
-            amount: 0
+            title: "Por fim, sua sobremesa",
+            options: [
+                {
+                    img: "/media/sobremesas/bombom.jpeg",
+                    name: "Bombom de Morango",
+                    description: "Com brigadeiro cremoso e casquinha de chocolate!",
+                    price: 3,
+                    amount: 0
+                },
+                {
+                    img: "/media/sobremesas/bolo.jpeg",
+                    name: "Bolo de Brigadeiro de Caramelo",
+                    description: "Nossa receita especial, recheado com Nutella.",
+                    price: 9,
+                    amount: 0
+                },
+                {
+                    img: "/media/sobremesas/brownie.jpeg",
+                    name: "Brownie",
+                    description: "Ideal para comer tomando um cafézinho.",
+                    price: 8,
+                    amount: 0
+                },
+                {
+                    img: "/media/sobremesas/cookie.jpeg",
+                    name: "Cookie de Chocotone",
+                    description: "Massa de laranja, com gotas de chocolate e recheado com Nutella!",
+                    price: 6,
+                    amount: 0
+                },
+                {
+                    img: "/media/sobremesas/pipoco.jpeg",
+                    name: "Pipoco",
+                    description: "Nosso brigadeiro que estoura na boca! Recheado com leite condensado.",
+                    price: 5,
+                    amount: 0
+                },
+                {
+                    img: "/media/sobremesas/gelatina.jpeg",
+                    name: "Gelatina de Morango Recheada",
+                    description: "Nossa famosa gelatina recheada de leite condensado.",
+                    price: 2.5,
+                    amount: 0
+                }
+            ]
         }
-    ];
-    const drinks = [
-        {
-            img: "/media/bebidas/coca.jpeg",
-            name: "Coca-Cola",
-            description: "Lata de 350ml.",
-            price: 5,
-            amount: 0
-        },
-        {
-            img: "/media/bebidas/kuat.jpeg",
-            name: "Guaraná Kuat",
-            description: "Lata de 350ml.",
-            price: 4,
-            amount: 0
-        },
-        {
-            img: "/media/bebidas/sprite.jpeg",
-            name: "Sprite",
-            description: "Lata de 350ml.",
-            price: 4.5,
-            amount: 0
-        },
-        {
-            img: "/media/bebidas/mate.jpeg",
-            name: "Chá Gelado Mate",
-            description: "Garaffa de 450ml sabor limão.",
-            price: 6,
-            amount: 0
-        },
-        {
-            img: "/media/bebidas/agua.jpeg",
-            name: "Água Mineral",
-            description: "Garaffa de 500ml sem gás.",
-            price: 1,
-            amount: 0
-        }
-    ];
-    const desserts = [
-        {
-            img: "/media/sobremesas/bombom.jpeg",
-            name: "Bombom de Morango",
-            description: "Com brigadeiro cremoso e casquinha de chocolate!",
-            price: 3,
-            amount: 0
-        },
-        {
-            img: "/media/sobremesas/bolo.jpeg",
-            name: "Bolo de Brigadeiro de Caramelo",
-            description: "Nossa receita especial, recheado com Nutella.",
-            price: 9,
-            amount: 0
-        },
-        {
-            img: "/media/sobremesas/brownie.jpeg",
-            name: "Brownie",
-            description: "Ideal para comer tomando um cafézinho.",
-            price: 8,
-            amount: 0
-        },
-        {
-            img: "/media/sobremesas/cookie.jpeg",
-            name: "Cookie de Chocotone",
-            description: "Massa de laranja, com gotas de chocolate e recheado com Nutella!",
-            price: 6,
-            amount: 0
-        },
-        {
-            img: "/media/sobremesas/pipoco.jpeg",
-            name: "Pipoco",
-            description: "Nosso brigadeiro que estoura na boca! Recheado com leite condensado.",
-            price: 5,
-            amount: 0
-        },
-        {
-            img: "/media/sobremesas/gelatina.jpeg",
-            name: "Gelatina de Morango Recheada",
-            description: "Nossa famosa gelatina recheada de leite condensado.",
-            price: 2.5,
-            amount: 0
-        }
-    ];
-
+    ]
+    
     return (
         <div>
             <Header />
-            <Category 
-                options={dishes}
-                title="Primeiro, seu prato"
-            />
-            <Category 
-                options={drinks}
-                title="Agora, sua bebida"
-            />
-            <Category 
-                options={desserts}
-                title="Por fim, sua sobremesa"
-            />
-            <Footer />
+            {menu.map((itens) => <Category title={itens.title} options={itens.options} />)}
+            <FinishOrder />
 
 
 
